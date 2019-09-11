8a695dc4e2d83103734a87d8c77e6fc2103fcaa8
Jhon Cruz <jhon.unal@gmail.com>
2019-09-11 18:47:19 -0500

    refactor + simple get endpoint

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const configMiddlewares = require('./lib/middlewares');
+const configRoutes = require('./lib/routes');
 
 var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app = configMiddlewares(app);
+app = configRoutes(app);
 
 module.exports = app;
