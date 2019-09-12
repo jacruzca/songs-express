1099f232d47d1c6146979ce0ffec8861a90bd5a6
Jhon Cruz <jhon.unal@gmail.com>
2019-09-11 19:35:45 -0500

    added eslint + fixed lint issues

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const configMiddlewares = require('./lib/middlewares');
-const configRoutes = require('./lib/routes');
-
-var app = express();
-app = configMiddlewares(app);
-app = configRoutes(app);
-
-module.exports = app;
